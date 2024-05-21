f26b6da1433323a03e77f92000851b7f13a6bf2c
George-Fx <64652126+George-Fx@users.noreply.github.com>
2024-05-21 21:29:38 +0400

    Update tokenData.js

--- src/controllers/tokenData.js
+++ src/controllers/tokenData.js
@@ -1,3 +1,16 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import {ethers} from 'ethers';
+
+import {abi} from '../abi/abi.js';
+
+dotenv.config();
+
+const RPC_URL = process.env.RPC_URL;
+const INFURA_MAINNET = process.env.INFURA_MAINNET;
+const COIN_GECKO_API_KEY = process.env.COIN_GECKO_API_KEY;
+const COIN_GECKO_SIMPLE_PRICE = 'https://api.coingecko.com/api/v3/simple/price';
+
 export const tokenData = async (req, res) => {
   console.log('this is test');
 };

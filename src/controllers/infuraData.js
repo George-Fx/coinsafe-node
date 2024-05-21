19d0b72817ba8b49abf0ed449882be1250340a03
George-Fx <64652126+George-Fx@users.noreply.github.com>
2024-05-21 21:02:44 +0400

    Update infuraData.js

--- src/controllers/infuraData.js
+++ src/controllers/infuraData.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
-import {ethers} from 'ethers';
+// import axios from 'axios';
+// import dotenv from 'dotenv';
+// import {ethers} from 'ethers';
 
-import {abi} from '../abi/abi.js';
+// import {abi} from '../abi/abi.js';
 
-dotenv.config();
+// dotenv.config();
 
-const RPC_URL = process.env.RPC_URL;
-const INFURA_MAINNET = process.env.INFURA_MAINNET;
-const COIN_GECKO_API_KEY = process.env.COIN_GECKO_API_KEY;
-const COIN_GECKO_SIMPLE_PRICE = 'https://api.coingecko.com/api/v3/simple/price';
+// const RPC_URL = process.env.RPC_URL;
+// const INFURA_MAINNET = process.env.INFURA_MAINNET;
+// const COIN_GECKO_API_KEY = process.env.COIN_GECKO_API_KEY;
+// const COIN_GECKO_SIMPLE_PRICE = 'https://api.coingecko.com/api/v3/simple/price';
 
 export const infuraData = async (req, res) => {
   // let providerUrl;
